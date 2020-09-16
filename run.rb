6a196c2905704240ae17246192bbea621cae2b67
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-09-16 18:36:12 +0900

    Create run.rb

---
+++ run.rb
@@ -0,0 +1,23 @@
+# やあ
+# やあ
+loop {
+print >
+command = gets.chomp
+if command == "yaa" then
+  puts "やあ"
+elsif command == "aaaaa" then
+  puts "ああああああああああああぁぁぁぁぁぁぁぁぁっ!!(発狂)"
+elsif command == "papertown" then
+  puts "紙町!紙町!"
+elsif command == "rakuten" then
+  puts "('ω')ﾗｧ⤴︎ｸﾃﾝ⤵︎ ︎ﾓﾊﾞｧ⤴︎ ｲﾙwwwww"
+elsif command == "weiwei" then
+  puts "(ง •ᴗ•)ว ⁾⁾ﾌｧｰｳｪｲでｳｪｲｳｪｲ"
+elsif command == "rairai" then
+  puts "(ง •ᴗ•)ว ⁾⁾ﾗｲｻﾝでﾗｲﾗｲ"
+elsif command == "exit" then
+  break
+  exit
+else
+  puts "おうおうおうおう"
+end
